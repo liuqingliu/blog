440a51a51783a18301c388db1275a06efc0f6d72
liuqing <liuqing@ludashi.com>
2018-04-18 10:44:18 +0800

    登录提示

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -17,6 +17,17 @@ class SessionsController extends Controller
     	$credentials = $this->validate($request, [
            'email' => 'required|email|max:255',
            'password' => 'required'
-       ]);
+        ]);
+
+        if (Auth::attempt($credentials)) {
+           session()->flash('success', '欢迎回来！');
+           return redirect()->route('users.show', [Auth::user()]);
+        } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back();
+        }
+
+       	return;
+
     }
 }
