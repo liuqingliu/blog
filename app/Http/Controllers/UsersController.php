77e63f7e2e2a4735a103c2eee1f06eaa8182dd86
liuqing <liuqing@ludashi.com>
2018-04-18 10:58:09 +0800

    导航栏登录注册

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -14,7 +15,7 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-    	return view('users.show', compact('user'));
+        return view('users.show', compact('user'));
     }
 
     public function store(Request $request)
@@ -31,9 +32,8 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-
         return redirect()->route('users.show', [$user]);
     }
-
 }
