ce05d183db7cb81700a7c266edd34402864acf3e
liuqing <liuqing@ludashi.com>
2018-04-18 19:32:36 +0800

    用户权限

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,8 +11,12 @@ class UsersController extends Controller
 
 	public function __construct()
     {
-        $this->middleware('auth', [            
+       $this->middleware('auth', [
             'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
         ]);
     }
 
@@ -47,6 +51,7 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+    	$this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
@@ -56,6 +61,8 @@ class UsersController extends Controller
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
         ]);
+
+        $this->authorize('update', $user);
 
         $data = [];
         $data['name'] = $request->name;
---
+++ app/Policies/AuthServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Providers;
+.
+,
+.
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\User::class  => \App\Policies\UserPolicy::class,
+    ];
+    .
+    .
+    .
+}
