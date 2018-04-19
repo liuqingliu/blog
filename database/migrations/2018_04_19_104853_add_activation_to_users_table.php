86dd50773ab8a29421c5dc2a4923df977fcb861d
liuqing <liuqing@ludashi.com>
2018-04-19 19:04:36 +0800

    active

--- database/migrations/2018_04_19_104853_add_activation_to_users_table.php
+++ database/migrations/2018_04_19_104853_add_activation_to_users_table.php
@@ -14,7 +14,8 @@ class AddActivationToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('activation_token')->nullable();
+            $table->boolean('activated')->default(false);
         });
     }
 
@@ -26,7 +27,8 @@ class AddActivationToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('activation_token');
+            $table->dropColumn('activated');
         });
     }
 }
