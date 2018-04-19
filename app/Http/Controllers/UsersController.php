45ca186c238a483ccfdc89a084f0aef4906a5709
liuqing <liuqing@ludashi.com>
2018-04-19 10:23:28 +0800

    bug

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,7 +23,7 @@ class UsersController extends Controller
     public function index()
     {
     	$users = User::paginate(10);
-    	return view('users.index', compact('user'));
+    	return view('users.index', compact('users'));
     }
 
     public function create()
